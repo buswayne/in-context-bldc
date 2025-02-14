963959ffb7882ce7e5547f5ebf7d5fe169fb24c9
Alessandro-Colombo <Alessandro50.colombo@mail.polimi.it>
2025-02-14 22:03:01 +0100

    new dataset generator

--- matlab_simulator/BLDC_simulator_init_other_pc2.m
+++ matlab_simulator/BLDC_simulator_init_other_pc2.m
@@ -6,24 +6,27 @@ pause on
 temp_name = strsplit(pwd,'in-context-bldc');
 % savepath = fullfile(temp_name{1}, "in-context-bldc","data","simulated\CL_speed_matlab\");
 
-perturbation_percent = 10;
+perturbation_percent = 50;
 
 perturbation = perturbation_percent / 100;
-savepath_tmp = "C:\Users\aless\OneDrive - Politecnico di Milano\in-context-bldc-data\simulated";
-folder_name = sprintf('%02.0f_percent_shorter_steps', perturbation_percent);
+savepath_tmp = "C:\Users\aless\OneDrive - Politecnico di Milano\in-context-bldc-data\simulated2";
+folder_name = sprintf('%02.0f_percent', perturbation_percent);
 savepath = fullfile(savepath_tmp, folder_name);
 [tmp, tmp2] = mkdir(savepath);
 
 % savepath = "C:\Users\39340\OneDrive - Politecnico di Milano\in-context-bldc-data\simulated\15_percent";
 speed_loop = 1;
 current_loop = 1;
-T = 10;
+
+
+
+T = 4.5;
 Ts = 1e-4;
 time = 0:Ts:T-Ts;
-Min_value = 0;
-Max_value = 3000; %in rpm
-Min_duration = 1;
-Max_duration = 3;
+% Min_value = 0;
+% Max_value = 3000; %in rpm
+% Min_duration = 1;
+% Max_duration = 3;
 
 
 N_exp = 1000;
@@ -33,16 +36,66 @@ mdl = 'BLDC_simulator2';
 
 for idx_exp = 1:N_exp
     fprintf("> simulating experiment %d out of %d \n", idx_exp, N_exp)
-
     now_string = string(datetime('now'),"yyyy-MM-dd_HH-mm-ss");
-    set_parameters_corrected_perturbed
+    flag_speed_check = true;
+
+    T = 0.5;
+    Ts = 1e-4;
+    time = 0:Ts:T-Ts;
+
+    while flag_speed_check
+    
+        set_parameters_corrected_perturbed
+        BLDC.RotorVelocityInit = 2000 /30 *pi /i_omega;
+
+
+        speed_input.time = time;
+        speed_input.signals.values = ones(length(time),1)*1e6;
+        load_input.time = time;
+        load_input.signals.values = zeros(length(time),1);
+        current_input.time = time;
+        current_input.signals.values = zeros(length(time),1);
+        voltage_d_input.time = time;
+        voltage_d_input.signals.values = zeros(length(time),1);
+        voltage_q_input.time = time;
+        voltage_q_input.signals.values = zeros(length(time),1);
+        output = sim(mdl);
+        final_speed = output.output.signals.values(end,2);
+        % figure
+        % hold on
+        % grid on
+        % plot(output.output.time, output.output.signals.values(:,2), "DisplayName","Omega")
+        % legend()
+        fprintf("   detected final speed: %d RPM \n", final_speed)
+        if (final_speed > 2000)
+            flag_speed_check = false;
+        else
+            fprintf("   discarding parameters set \n")
+        end
+    end
+    
+    max_speed = min(final_speed, 4000);
+    BLDC.RotorVelocityInit = 0;
+
+
+
+
+    
     % set_parameters
     
-    reference_speed = step_sequence(T, Ts, Min_value, Max_value, Min_duration, Max_duration);
+    T = 5;
+    Ts = 1e-4;
+    time = 0:Ts:T-Ts;
+    reference_speed = time * 0;
+    reference_speed(time <= 4.5) = rand() * max_speed;
+    reference_speed(time <= 2.5) = rand() * max_speed;
+    reference_speed(time <= 0.5) = 0;
+
     reference_speed = reference_speed / 30 * pi; %in rad/s
     
+
     speed_input.time = time;
-    speed_input.signals.values = reference_speed;
+    speed_input.signals.values = reference_speed';
     load_input.time = time;
     load_input.signals.values = zeros(length(time),1);
     current_input.time = time;
@@ -78,7 +131,7 @@ for idx_exp = 1:N_exp
     % if rem(idx_exp, 100) == 0
     %     pause(1200)
     % end
-    
+    % 
     % figure
     % ax1 = subplot(3,1,1);
     % hold on
