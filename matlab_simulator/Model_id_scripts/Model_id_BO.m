1110f4bfb8f3112f571f5a139a77bebe948b762b
Alessandro-Colombo <Alessandro50.colombo@mail.polimi.it>
2025-03-26 14:19:05 +0100

    ekf tuning on real data wip

--- matlab_simulator/Model_id_scripts/Model_id_BO.m
+++ matlab_simulator/Model_id_scripts/Model_id_BO.m
@@ -8,10 +8,10 @@ temp_name = strsplit(pwd,'in-context-bldc');
 savepath = fullfile(temp_name{1}, "in-context-bldc","data","simulated\CL_speed_matlab\");
 now_string = string(datetime('now'),"yyyy-MM-dd_HH-mm-ss");
 name = now_string + "_BO_result.mat";
-bayes_save_file_name = fullfile(temp_name{1}, "in-context-bldc", "matlab_simulator","BO_results", name);
+bayes_save_file_name = fullfile(temp_name{1}, "in-context-bldc", "matlab_simulator","BO_results_final", name);
 name_tmp = now_string + "_BO_result_tmp.mat";
 bayes_save_file_name_tmp = fullfile(temp_name{1}, "in-context-bldc", "matlab_simulator","BO_results", name_tmp);
-real_data_path = fullfile(temp_name{1}, "in-context-bldc","data","CL_experiments_double_sensor\train\inertia13_ki-0.0061-kp-11.8427\2025-03-03--14-51-19_exp   1.csv");
+real_data_path = fullfile(temp_name{1}, "in-context-bldc","data","CL_experiments_double_sensor_low_speed\final\inertia13_ki-0.0029-kp-3.0000\2025-03-25--17-26-19_exp  10.csv");
 % real_data_path = "C:\Users\aless\OneDrive - Politecnico di Milano\in-context-bldc-data\simulated_current_with_alfa_beta_new\Experiment__2025-03-17_09-17-14_i_omega_2_4970.csv";
 % real_data_path = "C:\Users\aless\OneDrive - Politecnico di Milano\in-context-bldc-data\low_speed_alpha_beta\Experiment_2025-03-24_09-33-56_i_omega_2_4970.csv";
 data = readtable(real_data_path);
@@ -37,14 +37,15 @@ output_list = [I_d, I_q];
 %Rs/Ls, 1/Ls, lambda_m/Ls, (3/2 * P * lambda_m) / J, B/J, theta0
 % 250, 700, 10, 200, 0.00001, ---
 
-p1 = optimizableVariable("p1",[150,700],"Type","real");
-p2 = optimizableVariable("p2",[50,550],"Type","real");
-p3 = optimizableVariable("p3",[4,50],"Type","real");
-p4 = optimizableVariable("p4",[400,800],"Type","real");
+p1 = optimizableVariable("p1",[100,400],"Type","real");
+p2 = optimizableVariable("p2",[10,200],"Type","real");
+p3 = optimizableVariable("p3",[2,50],"Type","real");
+p4 = optimizableVariable("p4",[50,1000],"Type","real");
 % p5 = optimizableVariable("p5",[0.000001,0.001],"Type","real");
 % p6 = optimizableVariable("p6",[-pi,+pi],"Type","real");
 
-fun = @(x)Model_Id_cost_function(x, input_list, output_list);
+% fun = @(x)Model_Id_cost_function(x, input_list, output_list);
+fun = @(x)Model_Id_cost_function_omega(x, input_list, omega);
 
 
 % result = bayesopt(fun, [p1,p2,p3,p4,p5,p6],"Verbose",2, ...
@@ -52,14 +53,14 @@ fun = @(x)Model_Id_cost_function(x, input_list, output_list);
 result = bayesopt(fun, [p1,p2,p3,p4],"Verbose",2, ...
     "AcquisitionFunctionName","expected-improvement-plus", ...
     "UseParallel",true, ...
-    NumSeedPoints=800, MaxObjectiveEvaluations=1500, ExplorationRatio=0.5);
+    NumSeedPoints=100, MaxObjectiveEvaluations=300, ExplorationRatio=0.5);
 save(bayes_save_file_name, "result")
 
 p(1) = result.XAtMinObjective.p1;
 p(2) = result.XAtMinObjective.p2;
 p(3) = result.XAtMinObjective.p3;
 p(4) = result.XAtMinObjective.p4;
-p(5) = result.XAtMinObjective.p5;
+% p(5) = result.XAtMinObjective.p5;
 % p(6) = result.XAtMinObjective.p6;
 
 result.XAtMinObjective
