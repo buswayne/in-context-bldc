c80b5544fa27cee6b68269e26be0280c0f0537e5
filippomattii <filippo.mattii@mail.polimi.it>
2025-07-21 13:35:57 +0200

    Update BLDC_simulator_init_high_speed.m

--- matlab_simulator/BLDC_simulator_init_high_speed.m
+++ matlab_simulator/BLDC_simulator_init_high_speed.m
@@ -7,10 +7,10 @@ close all
 tic
 temp_name = strsplit(pwd,'in-context-bldc');
 
-perturbation_percent = 50;
+perturbation_percent = 0;
 
 perturbation = perturbation_percent / 100;
-savepath_tmp = "C:\Users\39340\OneDrive - Politecnico di Milano\in-context-bldc-data\simulated";
+savepath_tmp = "C:/Users/81C3009S/OneDrive/Desktop/Tesi/data/rand_traj";
 folder_name = sprintf('%02.0f_percent_high_speed', perturbation_percent);
 savepath = fullfile(savepath_tmp, folder_name);
 [tmp, tmp2] = mkdir(savepath);
@@ -23,7 +23,7 @@ Ts = 1e-4;
 time = 0:Ts:T-Ts;
 
 
-N_exp = 1;
+N_exp = 200;
 
 mdl = 'BLDC_simulator';
 
@@ -32,42 +32,45 @@ for idx_exp = 1:N_exp
     now_string = string(datetime('now'),"yyyy-MM-dd_HH-mm-ss");
     flag_speed_check = true;
 
-    T = 1;
-    Ts = 1e-4;
-    time = 0:Ts:T-Ts;
+    % T = 1;
+    % Ts = 1e-4;
+    % time = 0:Ts:T-Ts;
 
+    set_parameters_perturbed2
 
     % some combination of perturbed parameters may lead to motor instancese
     % in which the maximum speed is low. Hence we briefly check
     % how fast can the motor go and we discard configurations that cannot
     % get at least 2000 rpm
-    while flag_speed_check
-    
-        set_parameters_perturbed
-        BLDC.RotorVelocityInit = 2000 /30 *pi /i_omega;
 
-        speed_input.time = time;
-        speed_input.signals.values = ones(length(time),1)*1e6;
-        load_input.time = time;
-        load_input.signals.values = zeros(length(time),1);
-        current_input.time = time;
-        current_input.signals.values = zeros(length(time),1);
-        voltage_d_input.time = time;
-        voltage_d_input.signals.values = zeros(length(time),1);
-        voltage_q_input.time = time;
-        voltage_q_input.signals.values = zeros(length(time),1);
-        output = sim(mdl);
-        final_speed = output.output.signals.values(end,2);
-
-        fprintf("   detected final speed: %d RPM \n", final_speed)
-        if (final_speed > 2000)
-            flag_speed_check = false;
-        else
-            fprintf("   discarding parameters set \n")
-        end
-    end
+    % while flag_speed_check
+    % 
+    %     set_parameters_perturbed2
+    %     BLDC.RotorVelocityInit = 2000 /30 *pi /i_omega;
+    % 
+    %     speed_input.time = time;
+    %     speed_input.signals.values = ones(length(time),1)*1e6;
+    %     load_input.time = time;
+    %     load_input.signals.values = zeros(length(time),1);
+    %     current_input.time = time;
+    %     current_input.signals.values = zeros(length(time),1);
+    %     voltage_d_input.time = time;
+    %     voltage_d_input.signals.values = zeros(length(time),1);
+    %     voltage_q_input.time = time;
+    %     voltage_q_input.signals.values = zeros(length(time),1);
+    %     output = sim(mdl);
+    %     final_speed = output.output.signals.values(end,2);
+    % 
+    %     fprintf("   detected final speed: %d RPM \n", final_speed)
+    %     if (final_speed > 2000)
+    %         flag_speed_check = false;
+    %     else
+    %         fprintf("   discarding parameters set \n")
+    %     end
+    % end
     
-    max_speed = min(final_speed, 4000);
+    %max_speed = min(final_speed, 4000);
+    max_speed = 3500;
     BLDC.RotorVelocityInit = 0;
 
     
@@ -75,8 +78,10 @@ for idx_exp = 1:N_exp
     Ts = 1e-4;
     time = 0:Ts:T-Ts;
     reference_speed = time * 0;
-    reference_speed(time <= 4.5) = rand() * max_speed;
-    reference_speed(time <= 2.5) = rand() * max_speed;
+    % reference_speed(time <= 4.5) = rand() * max_speed;
+    % reference_speed(time <= 2.5) = rand() * max_speed;
+    reference_speed(time <= 4.5) = 0.23 * max_speed;
+    reference_speed(time <= 2.5) = 0.67 * max_speed;
     reference_speed(time <= 0.5) = 0;
 
     reference_speed = reference_speed / 30 * pi; %in rad/s
@@ -111,30 +116,30 @@ for idx_exp = 1:N_exp
     exp_name = "Experiment_" + now_string + "_i_omega_" + str_speed + ".csv";
     writetable(out_tab,fullfile(savepath,exp_name));
  
-    figure
-    ax1 = subplot(3,1,1);
-    hold on
-    grid on
-    plot(output.output.time, output.output.signals.values(:,3), "DisplayName","Omega ref")
-    plot(output.output.time, output.output.signals.values(:,2), "DisplayName","Omega")
-    legend(["Omega ref", "Omega"])
-
-
-    ax2 = subplot(3,1,2);
-    hold on
-    grid on
-    plot(output.output.time, output.output.signals.values(:,6), "DisplayName","iq ref")
-    plot(output.output.time, output.output.signals.values(:,5), "DisplayName","iq")
-    plot(output.output.time, output.output.signals.values(:,4), "DisplayName","id")
-    legend()
-
-    ax3 = subplot(3,1,3);
-    hold on
-    grid on
-    plot(output.output.time, output.output.signals.values(:,7), "DisplayName","vd")
-    plot(output.output.time, output.output.signals.values(:,8), "DisplayName","vq")
-    legend()
-    linkaxes([ax1, ax2, ax3], 'x')
+    % figure
+    % ax1 = subplot(3,1,1);
+    % hold on
+    % grid on
+    % plot(output.output.time, output.output.signals.values(:,3), "DisplayName","Omega ref")
+    % plot(output.output.time, output.output.signals.values(:,2), "DisplayName","Omega")
+    % legend(["Omega ref", "Omega"])
+    % 
+    % 
+    % ax2 = subplot(3,1,2);
+    % hold on
+    % grid on
+    % plot(output.output.time, output.output.signals.values(:,6), "DisplayName","iq ref")
+    % plot(output.output.time, output.output.signals.values(:,5), "DisplayName","iq")
+    % plot(output.output.time, output.output.signals.values(:,4), "DisplayName","id")
+    % legend()
+    % 
+    % ax3 = subplot(3,1,3);
+    % hold on
+    % grid on
+    % plot(output.output.time, output.output.signals.values(:,7), "DisplayName","vd")
+    % plot(output.output.time, output.output.signals.values(:,8), "DisplayName","vq")
+    % legend()
+    % linkaxes([ax1, ax2, ax3], 'x')
 end
 
 toc
