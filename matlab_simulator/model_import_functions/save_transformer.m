140092baf45c66efb87920d00717b3e51c7a4089
Alessandro-Colombo <Alessandro50.colombo@mail.polimi.it>
2025-09-04 16:27:05 +0200

    wip transfering controller v2 models

--- matlab_simulator/model_import_functions/save_transformer.m
+++ matlab_simulator/model_import_functions/save_transformer.m
@@ -6,11 +6,11 @@ clc
 temp_name = strsplit(pwd,'in-context-bldc');
 temp_name2 = fullfile(temp_name{1},'in-context-bldc/speed_controller');
 
-model_name = 'ndp_noise_h10_40k_weights.pkl';
-model_name_to_save = 'ndp_noise_h10_40k';
+model_name = 'noise_h10_40k_weights.pkl';
+model_name_to_save = 'noise_h10_40k';
 
 network_path = fullfile(temp_name2, model_name);
-save_path = fullfile(temp_name{1},'in-context-bldc/matlab_simulator/networks');
+save_path = fullfile(temp_name{1},'in-context-bldc/matlab_simulator/networks2');
 [net, H] = import_transformer_model(network_path);
 model_name_to_save = model_name_to_save + "H" + num2str(H) + "H" + ".mat";
 save(fullfile(save_path, model_name_to_save), "net")
