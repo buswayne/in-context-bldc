4d4cad8cd831333fc46511a22431eaa669d38abc
Alessandro-Colombo <Alessandro50.colombo@mail.polimi.it>
2025-12-03 22:21:53 +0100

    figs for paper

---
+++ matlab_simulator/plot_comparison.m
@@ -0,0 +1,235 @@
+clc
+clear
+close all
+
+
+
+list_factory = fieldnames(get(groot,'factory'));
+index_interpreter = find(contains(list_factory,'Interpreter'));
+for i = 1:length(index_interpreter)
+    default_name = strrep(list_factory{index_interpreter(i)},'factory','default');
+    set(groot, default_name,'latex');
+end
+
+inertia = 15;
+
+alpha = 0.6;
+
+
+color_tr = [     0 0.4196 0.6510, alpha];
+color_bo = [0.8663 0.4061 0.2384, alpha];
+color_vrft = [0.4846 0.7497 0.5805, alpha];
+
+
+
+
+
+fprintf("inertia %d\n", inertia)
+
+
+
+switch inertia
+    case 13
+
+
+        tab_tr = readtable("../data/transformer_v2_CAN_exp/new_dataset_short_noise_h10_30k_H10H13/I13_2025-11-11--11-13-39.csv");
+        metadata = load("../data/transformer_v2_CAN_exp/new_dataset_short_noise_h10_30k_H10H13/I13_2025-11-11--11-13-39_metadata.mat");
+        
+        
+        tab_bo = readtable("../data/CL_experiments_benchmark_BO/inertia_13__ki-0.0000-kp-28.8122/I_13__2025-11-27--10-04-17.csv");
+        tab_vrft = readtable("../data/CL_experiments_benchmark_VRFT/inertia_13__ki-0.0000-kp-2.8007/I_13__2025-11-27--12-17-56.csv");
+
+
+    case 5
+
+
+        tab_tr = readtable("../data/transformer_v2_CAN_exp/new_dataset_short_noise_h10_30k_H10H05/I05_2025-11-11--11-41-35.csv");
+        metadata = load("../data/transformer_v2_CAN_exp/new_dataset_short_noise_h10_30k_H10H05/I05_2025-11-11--11-41-35_metadata.mat");
+        
+        
+        tab_bo = readtable("../data/CL_experiments_benchmark_BO/inertia_05__ki-0.0152-kp-15.9433/I_05__2025-11-27--14-44-53.csv");
+        % tab_vrft = readtable("../data/CL_experiments_benchmark_VRFT/inertia_");
+    
+    case 15
+
+
+        tab_tr = readtable("../data/transformer_v2_CAN_exp/new_dataset_short_noise_h10_30k_H10H15/I15_2025-11-11--12-10-34.csv");
+        metadata = load("../data/transformer_v2_CAN_exp/new_dataset_short_noise_h10_30k_H10H15/I15_2025-11-11--12-10-34_metadata.mat");
+        
+        
+        tab_bo = readtable("../data/CL_experiments_benchmark_BO/inertia_15__ki-0.0000-kp-11.9834/I_15__2025-11-27--14-48-46.csv");
+        tab_vrft = readtable("../data/CL_experiments_benchmark_VRFT/inertia_15__ki-0.0000-kp-2.9085/I_15__2025-11-27--14-54-14.csv");
+
+    case 9
+
+
+        tab_tr = readtable("../data/transformer_v2_CAN_exp/new_dataset_short_noise_h10_30k_H10H09/I09_2025-11-11--14-42-05.csv");
+        metadata = load("../data/transformer_v2_CAN_exp/new_dataset_short_noise_h10_30k_H10H09/I09_2025-11-11--14-42-05_metadata.mat");
+        
+        
+        tab_bo = readtable("../data/CL_experiments_benchmark_BO/inertia_09__ki-0.1000-kp-50.0000/I_09__2025-11-27--15-22-39.csv");
+        tab_vrft = readtable("../data/CL_experiments_benchmark_VRFT/inertia_09__ki-0.0000-kp-3.8716/I_09__2025-11-27--15-21-10.csv");
+
+    case 11
+
+        tab_tr = readtable("../data/transformer_v2_CAN_exp/new_dataset_short_noise_h10_30k_H10H11/I11_2025-11-11--15-10-53.csv");
+        metadata = load("../data/transformer_v2_CAN_exp/new_dataset_short_noise_h10_30k_H10H11/I11_2025-11-11--15-10-53_metadata.mat");
+        
+        
+        tab_bo = readtable("../data/CL_experiments_benchmark_BO/inertia_11__ki-0.0000-kp-43.1595/kp_0.0000_ki_43.1595.csv");
+        tab_vrft = readtable("../data/CL_experiments_benchmark_VRFT/inertia_11__ki-0.0000-kp-3.8905/I_11__2025-11-27--15-36-43.csv");
+
+    case 7
+
+        tab_tr = readtable("../data/transformer_v2_CAN_exp/new_dataset_short_noise_h10_30k_H10H07/I07_2025-11-11--15-31-17.csv");
+        metadata = load("../data/transformer_v2_CAN_exp/new_dataset_short_noise_h10_30k_H10H07/I07_2025-11-11--15-31-17_metadata.mat");
+        
+        
+        tab_bo = readtable("../data/CL_experiments_benchmark_BO/inertia_07__ki-0.0000-kp-50.0000/I_07__2025-11-27--15-30-24.csv");
+        tab_vrft = readtable("../data/CL_experiments_benchmark_VRFT/inertia_07__ki-0.0000-kp-2.8185/I_07__2025-11-27--15-28-58.csv");
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+% r = tab_tr.r;
+omega = tab_tr.omega;
+
+% start_idx = max(1, find(r>1800,1,"first") - 1);
+start_idx = max(1, find(omega>50,1,"first") - 4);
+end_idx = start_idx + 500;
+
+
+omega_cut = tab_tr.omega(start_idx:end_idx);
+% vd_cut = tab_tr.vd(start_idx:end_idx);
+% vq_cut = tab_tr.vq(start_idx:end_idx);
+% id_cut = tab_tr.id(start_idx:end_idx);
+iq_cut = tab_tr.iq(start_idx:end_idx);
+% iq_ref_cut = tab_tr.iq_ref(start_idx:end_idx);
+% r_cut = tab_tr.r(start_idx:end_idx);
+t_cut = tab_tr.t(start_idx:end_idx);
+t_cut = t_cut - t_cut(1);
+Ts = t_cut(2)-t_cut(1);
+
+
+OS_percent = metadata.metadata.OS;
+T_set = metadata.metadata.T_s;
+
+s = tf('s');
+z = tf('z',Ts);
+z.Variable = 'z^-1';
+
+OS = OS_percent / 100;
+zeta = -log(OS) / sqrt(pi^2 + (log(OS))^2);
+wn = 3 / (zeta * T_set);
+num = [wn^2];
+den = [1, 2*zeta*wn, wn^2];
+fprintf("Overshoot: %f%%\n", OS_percent)
+fprintf("Settling time: %fs\n", T_set)
+
+
+
+M = tf(num, den);
+M = c2d(M,Ts,'tustin');
+
+
+
+[y_model, t_model] = step(2000*M, 0:Ts:5);
+
+try
+    iq_ref = tab_bo.iq_ref;
+    start_idx = max(1, find(iq_ref>0.1,1,"first") - 1);
+    end_idx = start_idx + 5000;
+
+catch
+    iq = tab_bo.iq;
+    start_idx = max(1, find(iq>0.1,1,"first") - 1);
+    end_idx = start_idx + 500;
+end
+
+
+omega_cut_bo = tab_bo.omega(start_idx:end_idx);
+iq_cut_bo = tab_bo.iq(start_idx:end_idx);
+time_bo = tab_bo.t(start_idx:end_idx) - tab_bo.t(start_idx);
+
+try
+    iq_ref = tab_vrft.iq_ref;
+    start_idx = max(1, find(iq_ref>0.1,1,"first") - 1);
+    end_idx = start_idx + 5000;
+    
+    omega_cut_vrft = tab_vrft.omega(start_idx:end_idx);
+    iq_cut_vrft = tab_vrft.iq(start_idx:end_idx);
+    time_vrft = tab_vrft.t(start_idx:end_idx) - tab_vrft.t(start_idx);
+catch
+    fprintf("No vrft found\n")
+end
+
+
+
+
+
+figure;
+hold on
+plot(t_cut, omega_cut, 'Color',color_tr, 'LineWidth',1, 'DisplayName', '$\mathcal{C}$')
+plot(t_model, y_model, '--', 'Color', 'k' , 'LineWidth',2, 'DisplayName', '$M$')
+try
+plot(time_vrft, omega_cut_vrft, 'Color',color_vrft, 'LineWidth',1, 'DisplayName', 'VRFT')
+catch
+end
+plot(time_bo, omega_cut_bo, 'Color',color_bo, 'LineWidth',1, 'DisplayName', 'BO')
+tit = sprintf('Step Response ($T_{set}\\approx %.2fs, OS_{\\%%}\\approx %.2f\\%%$)', T_set, OS_percent);
+title(tit, 'Interpreter','latex');
+grid on;
+xlim([0,1.5])
+ylim([0,2500])
+legend('Location','southeast')
+
+
+
+
+
+figure('Position',[200,200,500,300])
+subplot(211)
+hold on
+box on
+plot(t_model, y_model, '--', 'Color','k', 'LineWidth',2, 'DisplayName', '$M$')
+try
+plot(time_vrft, omega_cut_vrft, 'Color',color_vrft, 'LineWidth',2, 'DisplayName', 'VRFT')
+catch
+end
+plot(time_bo, omega_cut_bo, 'Color',color_bo, 'LineWidth',2, 'DisplayName', 'BO')
+plot(t_cut, omega_cut, 'Color',color_tr, 'LineWidth',2, 'DisplayName', '$\mathcal{C}$')
+tit = sprintf('Step Response ($T_{set}\\approx %.2fs, OS_{\\%%}\\approx %.2f\\%%$)', T_set, OS_percent);
+title(tit, 'Interpreter','latex');
+ylabel("$\omega$  [rpm]")
+xlim([0,1.5])
+ylim([0,2500])
+legend('Location','southeast')
+
+subplot(212)
+hold on
+box on
+try
+plot(time_vrft, iq_cut_vrft, 'Color',color_vrft, 'LineWidth',2, 'DisplayName', 'VRFT')
+catch
+end
+plot(time_bo, iq_cut_bo, 'Color',color_bo, 'LineWidth',2, 'DisplayName', 'BO')
+plot(t_cut, iq_cut, 'Color',color_tr, 'LineWidth',2, 'DisplayName', '$\mathcal{C}$')
+ylabel("$i_q$ [A]")
+xlabel("Time [s]")
+xlim([0,1.5])
+% ylim([0,2500])
+legend()
+
+
--- matlab_simulator/plot_response_stack_steps2.m
+++ matlab_simulator/plot_response_stack_steps2.m
@@ -1,7 +1,6 @@
 clear
 clc
-% close all
-
+close all
 
 
 list_factory = fieldnames(get(groot,'factory'));
@@ -27,12 +26,12 @@ color_stack = strings(0);
 color_stack_good = strings(0);
 stepsize = 2000;
 
-% colors_good = {"#023E8A", "#03045E", "#0077B6", "#0096C7", "#00B4D8", "#48CAE4"};
 
-% colors_good = color_tints_and_shades(cell2mat(hex2rgb("#0077B6")),10,0.5);
 colors_good = color_tints_and_shades([0    0.4471    0.7412],8,0.65);
 
-% 0    0.4471    0.7412
+
+
+
 
 temp_name = strsplit(pwd,'in-context-bldc');
 user_tmp = strsplit(pwd,'Users\');
@@ -41,7 +40,7 @@ user = user_tmp2{1};
 
 
 
-datapath = fullfile(temp_name{1}, 'in-context-bldc', 'data', 'transformer_v2_CAN_exp');
+datapath = fullfile(temp_name{1}, 'in-context-bldc', 'data', 'transformer_v2_CAN_exp_var_step');
 
 
 folder_list = dir(sprintf('%s',datapath));
@@ -137,11 +136,16 @@ for i = 1:n_models
             for jj = 1:length(current_file_list)
 
                 current_file = fullfile(current_file_list(jj).folder, current_file_list(jj).name);
+                if contains(current_file, 'step500_')
+                    continue
+                end
+
                 res_tmp = readtable(current_file);
                 r = res_tmp.r;
-                start_idx = max(1, find(r>1800,1,"first") - 1);
+                iq_ref = res_tmp.iq_ref;
+                start_idx = max(1, find(iq_ref>0.1,1,"first") - 1);
                 % end_idx = min(length(r), find(r>1800,1,"last"));
-                end_idx = start_idx + 500;
+                end_idx = start_idx + 300;
 
                 time_stack(end+1,:) = res_tmp.t(start_idx:end_idx) - res_tmp.t(start_idx);
                 omega_stack(end+1,:) = res_tmp.omega(start_idx:end_idx);
@@ -247,6 +251,11 @@ end %% n_models
 figure('Position',[100,100,500,200])
 hold on
 box on
+% for j = 1:n_exp
+%     plot(time_stack(j,:), omega_stack(j,:), 'Color',color_stack_good(j), 'LineWidth',1)
+% end
+
+
 for j = flip(1:n_exp)
     if color_stack_good(j) == rgb2hex(colors_good{1})
         plot(time_stack(j,:), omega_stack(j,:), 'Color',color_stack_good(j))
@@ -277,101 +286,9 @@ for j = flip(1:n_exp)
         plot(time_stack(j,:), omega_stack(j,:), 'Color',color_stack_good(j))
     end
 end
-xlim([0,2])
-ylim([0,2500])
 
 % r = rectangle('Position',[1.5, 0, 5, 1900], 'FaceColor',"b","EdgeColor","none","FaceAlpha",0.1);
 % hatchfill(r, 'single',-45, 'b');
-x_corners = [1.5, 5, 5, 1.5]; 
-y_corners = [0, 0, 1900, 1900];
-p = patch(x_corners, y_corners, 'r', 'FaceColor', '#adb5bd', 'EdgeColor', 'none', 'FaceAlpha', 0.3);
-patch(x_corners, y_corners, 'r', 'FaceColor', '#adb5bd', 'EdgeColor', 'none', 'FaceAlpha', 0.1);
-hh = hatchfill(p, 'single', 45, 5);
-set(hh, 'Color', '#adb5bd', 'LineWidth', 0.5)
-
-x_corners = [1.5, 5, 5, 1.5]; 
-y_corners = [2100, 2100, 3000, 3000];
-p = patch(x_corners, y_corners, 'r', 'FaceColor', '#adb5bd', 'EdgeColor', 'none', 'FaceAlpha', 0.3);
-patch(x_corners, y_corners, 'r', 'FaceColor', '#adb5bd', 'EdgeColor', 'none', 'FaceAlpha', 0.1);
-hh = hatchfill(p, 'single', 45, 5);
-set(hh, 'Color', '#adb5bd', 'LineWidth', 0.5)
-
-
-x_corners = [-0, 1.5, 1.5, -0]; 
-y_corners = [2400, 2400, 3000, 3000];
-p = patch(x_corners, y_corners, 'r', 'FaceColor', '#adb5bd', 'EdgeColor', 'none', 'FaceAlpha', 0.3);
-patch(x_corners, y_corners, 'r', 'FaceColor', '#adb5bd', 'EdgeColor', 'none', 'FaceAlpha', 0.1);
-hh = hatchfill(p, 'single', 45, 5);
-set(hh, 'Color', '#adb5bd', 'LineWidth', 0.5)
-
-xlabel("Time [s]")
-ylabel("$\omega$ [rpm]")
-
-% 
-L1 = plot(nan, nan, 'color', colors_good{1}, 'LineWidth',2);
-L2 = plot(nan, nan, 'color', colors_good{2}, 'LineWidth',2);
-L3 = plot(nan, nan, 'color', colors_good{3}, 'LineWidth',2);
-L4 = plot(nan, nan, 'color', colors_good{4}, 'LineWidth',2);
-L5 = plot(nan, nan, 'color', colors_good{5}, 'LineWidth',2);
-L6 = plot(nan, nan, 'color', colors_good{6}, 'LineWidth',2);
-% L1 = plot(nan, nan, 'color', "#FAA307", 'LineWidth',2);
-% L2 = plot(nan, nan, 'color', "#FFBA08", 'LineWidth',2);
-% L3 = plot(nan, nan, 'color', "#F48C06", 'LineWidth',2);
-% L4 = plot(nan, nan, 'color', "#E85D04", 'LineWidth',2);
-% L5 = plot(nan, nan, 'color', "#DC2F02", 'LineWidth',2);
-% L6 = plot(nan, nan, 'color', "#D00000", 'LineWidth',2);
-% L1 = plot(nan, nan, 'color', H(1), 'LineWidth',2);
-% L2 = plot(nan, nan, 'color', H(2), 'LineWidth',2);
-% L3 = plot(nan, nan, 'color', H(3), 'LineWidth',2);
-% L4 = plot(nan, nan, 'color', H(4), 'LineWidth',2);
-% L5 = plot(nan, nan, 'color', H(5), 'LineWidth',2);
-% L6 = plot(nan, nan, 'color', H(6), 'LineWidth',2);
-legend([L1, L2,L3,L4,L5,L6], {'$S^{(1)}$', '$S^{(2)}$', '$S^{(3)}$', '$S^{(4)}$' ,'$S^{(5)}$' ,'$S^{(6)}$'}, 'Location','southeast')
-
-savefig("figs_paper/stack_results.fig")
-saveas(gcf, "figs_paper/stack_results.png")
-fig = gcf;
-set(fig, 'PaperPositionMode', 'auto');
-exportgraphics(fig, 'figs_paper/stack_results.pdf', 'ContentType', 'vector');
-
-
-
-
-% 
-% figure
-% hold on
-% 
-% load("results_simulator_stack.mat")
-% 
-% [n_exp_sim, t_len] = size(out.omegas);
-% 
-% mask = 1:10:n_exp_sim;
-% 
-% n_colors = n_exp_sim;
-% 
-% 
-% start_color_g = [0, 0.5, 0];
-% end_color_g   = [0.5, 0.8, 0.5];
-% 
-% % Create the gradient for each channel
-% r_channel = linspace(start_color_g(1), end_color_g(1), n_colors)';
-% g_channel = linspace(start_color_g(2), end_color_g(2), n_colors)';
-% b_channel = linspace(start_color_g(3), end_color_g(3), n_colors)';
-% colors = [r_channel, g_channel, b_channel];
-% 
-% for j = 1:10:n_exp_sim
-%     plot(out.time, out.omegas(j,:), 'Color',colors(j,:),'LineWidth',0.1);
-%     % alpha(p, 0.5)
-% end
-% 
-% 
-% 
-% for j = 1:n_exp
-%     plot(time_stack(j,:), omega_stack(j,:), 'Color',color_stack_good(j))
-% end
-% 
-% % r = rectangle('Position',[1.5, 0, 5, 1900], 'FaceColor',"b","EdgeColor","none","FaceAlpha",0.1);
-% % hatchfill(r, 'single',-45, 'b');
 % x_corners = [1.5, 5, 5, 1.5]; 
 % y_corners = [0, 0, 1900, 1900];
 % p = patch(x_corners, y_corners, 'r', 'FaceColor', 'b', 'EdgeColor', 'none', 'FaceAlpha', 0.3);
@@ -393,30 +310,36 @@ exportgraphics(fig, 'figs_paper/stack_results.pdf', 'ContentType', 'vector');
 % patch(x_corners, y_corners, 'r', 'FaceColor', 'b', 'EdgeColor', 'none', 'FaceAlpha', 0.1);
 % hh = hatchfill(p, 'single', 45, 5);
 % set(hh, 'Color', 'b', 'LineWidth', 0.5)
-% 
-% xlim([0,5])
-% ylim([0,2500])
-% xlabel("Time [s]")
-% ylabel("Speed [rpm]")
-% 
-% 
-% % 
+
+xlim([0,1.5])
+ylim([0,3000])
+xlabel("Time [s]")
+ylabel("$\omega$ [rpm]")
+
+
+L1 = plot(nan, nan, 'color', colors_good{1}, 'LineWidth',2);
+L2 = plot(nan, nan, 'color', colors_good{2}, 'LineWidth',2);
+L3 = plot(nan, nan, 'color', colors_good{3}, 'LineWidth',2);
+L4 = plot(nan, nan, 'color', colors_good{4}, 'LineWidth',2);
+L5 = plot(nan, nan, 'color', colors_good{5}, 'LineWidth',2);
+L6 = plot(nan, nan, 'color', colors_good{6}, 'LineWidth',2);
 % L1 = plot(nan, nan, 'color', "#FAA307", 'LineWidth',2);
 % L2 = plot(nan, nan, 'color', "#FFBA08", 'LineWidth',2);
 % L3 = plot(nan, nan, 'color', "#F48C06", 'LineWidth',2);
 % L4 = plot(nan, nan, 'color', "#E85D04", 'LineWidth',2);
 % L5 = plot(nan, nan, 'color', "#DC2F02", 'LineWidth',2);
 % L6 = plot(nan, nan, 'color', "#D00000", 'LineWidth',2);
-% % L1 = plot(nan, nan, 'color', H(1), 'LineWidth',2);
-% % L2 = plot(nan, nan, 'color', H(2), 'LineWidth',2);
-% % L3 = plot(nan, nan, 'color', H(3), 'LineWidth',2);
-% % L4 = plot(nan, nan, 'color', H(4), 'LineWidth',2);
-% % L5 = plot(nan, nan, 'color', H(5), 'LineWidth',2);
-% % L6 = plot(nan, nan, 'color', H(6), 'LineWidth',2);
-% legend([L1, L2,L3,L4,L5,L6], {'Config 1', 'Config 2', 'Config 3', 'Config 4' ,'Config 5' ,'Config 6'}, 'Location','southeast')
-% 
-% savefig("figs_paper/stack_results_and_sim.fig")
-% saveas(gcf, "figs_paper/stack_results_and_sim.png")
-% fig = gcf;
-% set(fig, 'PaperPositionMode', 'auto');
-% exportgraphics(fig, 'figs_paper/stack_results_and_sim.pdf', 'ContentType', 'vector');
+% L1 = plot(nan, nan, 'color', H(1), 'LineWidth',2);
+% L2 = plot(nan, nan, 'color', H(2), 'LineWidth',2);
+% L3 = plot(nan, nan, 'color', H(3), 'LineWidth',2);
+% L4 = plot(nan, nan, 'color', H(4), 'LineWidth',2);
+% L5 = plot(nan, nan, 'color', H(5), 'LineWidth',2);
+% L6 = plot(nan, nan, 'color', H(6), 'LineWidth',2);
+legend([L1, L2,L3,L4,L5,L6], {'$S^{(1)}$', '$S^{(2)}$', '$S^{(3)}$', '$S^{(4)}$' ,'$S^{(5)}$' ,'$S^{(6)}$'}, 'Location','southeast')
+
+savefig("figs_paper/stack_multistep.fig")
+saveas(gcf, "figs_paper/stack_multistep.png")
+fig = gcf;
+set(fig, 'PaperPositionMode', 'auto');
+exportgraphics(fig, 'figs_paper/stack_multistep.pdf', 'ContentType', 'vector');
+
